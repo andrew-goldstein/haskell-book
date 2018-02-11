e48f4ddd598b1d07d70e0bfb28e61091d05a90d6
Andrew Goldstein <abgoldst@gmail.com>
2018-02-10 18:35:27 -0700

    * completed ch5 * fixup README

---
+++ ch5/typeKwonDo2.hs
@@ -0,0 +1,10 @@
+module TypeKwonDo2 where
+
+f :: Int -> String
+f = undefined
+
+g :: String -> Char
+g = undefined
+
+h :: Int -> Char
+h x = g (f x)
---
+++ ch5/typeKwonDo3.hs
@@ -0,0 +1,14 @@
+module TypeKwonDo3 where
+
+data A
+data B
+data C
+
+q :: A -> B
+q = undefined
+
+w :: B -> C
+w = undefined
+
+e :: A -> C
+e x = w (q x)
